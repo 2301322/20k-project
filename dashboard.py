9261b815346ffbdb12ce19f8fad6cb442fa02769
Anson <anson.t@digipen.edu>
2025-03-12 16:13:30 +0800

    Push

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,11 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+from data_scraper import scrape_reddit, scrape_twitter
+from sentiment_analysis import process_feedback
+from topic_modelling import extract_topics
+import pandas as pd
+
 
 def generate_wordcloud(text_data):
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate(" ".join(text_data))
@@ -13,6 +18,7 @@ def generate_wordcloud(text_data):
 def main():
     st.title("Game Balance Sentiment Dashboard")
     st.subheader("Visualising balance concerns in games")
+    
     feedback_data = scrape_reddit() + scrape_twitter()
     processed_data = process_feedback(feedback_data)
     st.dataframe(processed_data)
